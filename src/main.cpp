efa4c209a5ddc7053332261894da1448c123056f
Mason <mason@aidsbooger.com>
2026-01-02 13:54:44 -0500

    Added basic jump and collision with platform

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,9 @@ int main()
     auto player = sf::RectangleShape({50, 100});
     player.setFillColor(sf::Color::Magenta);
     player.setPosition({935, 500});
+    bool player_can_jump = false;
+
+    sf::Vector2f player_velocity(0, 0);
 
     auto platform = sf::RectangleShape({1500, 10});
     platform.setPosition({210, 1000});
@@ -27,8 +30,26 @@ int main()
             if (event->is<sf::Event::KeyReleased>()) { heldKeys[event->getIf<sf::Event::KeyReleased>()->code] = false; }
         }
 
+        // handle horizontal movement
         if (heldKeys.count(sf::Keyboard::Key::Left) > 0 && heldKeys[sf::Keyboard::Key::Left]) player.setPosition(player.getPosition() + sf::Vector2f(-5, 0));
         if (heldKeys.count(sf::Keyboard::Key::Right) > 0 && heldKeys[sf::Keyboard::Key::Right]) player.setPosition(player.getPosition() + sf::Vector2f(5, 0));
+
+        // handle gravity
+        player_velocity += sf::Vector2f(0, 10. / 144);
+        player.setPosition(player.getPosition() + player_velocity);
+        if (player.getGlobalBounds().findIntersection(platform.getGlobalBounds()).has_value())
+        {
+            player_velocity.y = 0;
+
+            auto top_of_platform = platform.getPosition().y;
+            auto bottom_of_player = player.getPosition().y + player.getSize().y;
+            auto diff = top_of_platform - bottom_of_player;
+            player.setPosition(player.getPosition() + sf::Vector2f(0, diff));
+            player_can_jump = true;
+        }
+        else { player_can_jump = false; }
+
+        if (player_can_jump && heldKeys.count(sf::Keyboard::Key::Space) > 0 && heldKeys[sf::Keyboard::Key::Space]) { player_velocity.y = -10; }
 
         window.clear();
         
